c345626afd3daef4c99caa026f1bb6e3908bef89
Ksenia Kozyreva <oestara91@gmail.com>
2020-07-11 15:31:05 +0300

    modified tests

--- src/test/java/OrderFormTest.java
+++ src/test/java/OrderFormTest.java
@@ -1,8 +1,7 @@
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -16,7 +15,7 @@ public class OrderFormTest {
         form.$("[data-test-id=phone] input").setValue("+79215555555");
         form.$("[data-test-id=agreement]").click();
         form.$("[role=button]").click();
-        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
@@ -27,7 +26,7 @@ public class OrderFormTest {
         form.$("[data-test-id=phone] input").setValue("+79215555555");
         form.$("[data-test-id=agreement]").click();
         form.$("[role=button]").click();
-        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
@@ -38,7 +37,7 @@ public class OrderFormTest {
         form.$("[data-test-id=phone] input").setValue("89456657687+");
         form.$("[data-test-id=agreement]").click();
         form.$("[role=button]").click();
-        $(".input_invalid").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
@@ -48,7 +47,7 @@ public class OrderFormTest {
         form.$("[data-test-id=phone] input").setValue("89456657687+");
         form.$("[data-test-id=agreement]").click();
         form.$("[role=button]").click();
-        $(" .input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -58,6 +57,17 @@ public class OrderFormTest {
         form.$("[data-test-id=name] input").setValue("Ксения Козырева");
         form.$("[data-test-id=agreement]").click();
         form.$("[role=button]").click();
-        $(" .input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
+
+    @Test
+    void CheckBoxTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Козырева Ксения");
+        form.$("[data-test-id=phone] input").setValue("+79215555555");
+        form.$("[role=button]").click();
+        $("[data-test-id=agreement]").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+    }
+
 }
